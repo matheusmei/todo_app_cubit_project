8d3c065c595163eac59f4cac869ab7e410214232
Matheus Mei <matheus.cmei@gmail.com>
2023-08-31 18:34:17 -0300

    filter cubit done

--- lib/cubits/filters/filters_cubit.dart
+++ lib/cubits/filters/filters_cubit.dart
@@ -1,11 +1,16 @@
-import 'package:bloc/bloc.dart';
+
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app_cubit_project/models/todo_model.dart';
 
 part 'filters_state.dart';
 
 class FiltersCubit extends Cubit<TodoFiltersState> {
   FiltersCubit() : super(TodoFiltersState.initial());
+
+  void changeFilter(Filters newFilter) {
+  emit(state.copyWith(filter: newFilter));
 }
 
+}
 
