7a9d750bbcd953434fac86349aafa754641cc7b2
Matheus Mei <matheus.cmei@gmail.com>
2023-09-04 16:11:40 -0300

    created activities cubit

---
+++ lib/cubits/activities_count/activities_count_cubit.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:todo_app_cubit_project/cubits/todo_list/todo_list_cubit.dart';
+import 'package:todo_app_cubit_project/models/todo_model.dart';
+
+part 'activities_count_state.dart';
+
+class ActivitiesCountCubit extends Cubit<ActivitiesCountState> {
+  late final StreamSubscription todoListSubscription;
+  final TodoListCubit todoListCubit;
+  ActivitiesCountCubit({
+    required this.todoListCubit,
+  }) : super(ActivitiesCountState.initial()) {
+    todoListSubscription =
+        todoListCubit.stream.listen((TodoListState todoListState) {
+      print('todoListState: $todoListState');
+    });
+
+    final int currentActivitiesCount = todoListCubit.state.todoList
+        .where((TodoModel activity) => !activity.isDone)
+        .toList()
+        .length;
+
+    emit(state.copyWith(activitiesCount: currentActivitiesCount));
+  }
+
+  @override
+  Future<void> close() {
+    todoListSubscription.cancel();
+    return super.close();
+  }
+
+}
